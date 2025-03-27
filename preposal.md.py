f537ef3c8aa5975ac8919ae3d1c550830f13884f
talifar <talhaafarooq@gmail.com>
2025-03-27 18:36:38 -0400

    Add files via upload

---
+++ preposal.md.py
@@ -0,0 +1,27 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+# Medical theme
+#   - symptom checker with basic patient record system: input symtoms, get diagnosis, save multiple patient symptoms and diagnoses
+#   - health metrics tracker: user inputs numbers, code calculates metrics and gives warnings
+#   - heart rate tracker and analysis: takes user input, calculates expected/average and compares it to user during different activities
+#   - basic ECG pattern recognizer: simulates or accepts input for heartbeat intervals, determines irregularities, classifies rhythms and graphs
+#   - rehab exercise tracker/planner: takes user input on injury and severity, gives rehab plan and tracks progress
+
+# Volleyball theme
+#   - stat analyzer: input game stats, calculates ratios and percentages, multiple games and players values, leaderboard, rank efficiencies
+#   - serve simulator/analyzer: simulates serves and gives stats, what zones produce most success, can be tailored for specific teams like game film
+
+# Other
+#   - upcoming project for R language in another class
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+# Individiual
+
+
+## Do you have any questions of your own?
+
+# I am not sure how complicated my ideas are or what level of skill I need to complete them, so I am not sure which is reasonable to do.
